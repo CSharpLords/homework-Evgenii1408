fc9d7772afee22fb55c6aa1296660aa957b9e560
Alex <iseldomused@gmail.com>
2017-11-14 03:57:43 +0300

    add 09

---
+++ 09/MemoryTrainer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemoryTrainer {
+	class Program {
+		static void Main(string[] args) {
+			GuessNumber();
+		}
+
+		static void GuessNumber() {
+			Random rand = new Random();
+
+			while (true) {
+				int number = rand.Next(100);
+
+				Console.WriteLine("Запомните число: " + number);
+				Thread.Sleep(2000);
+				Console.Clear();
+
+				Console.WriteLine("Введите запомненное число");
+				int guess = int.Parse(Console.ReadLine());
+				Console.WriteLine();
+
+				if (guess == number) {
+					Console.WriteLine("Вы угадали!");
+				}
+				else {
+					Console.WriteLine("Вы ошиблись ='(");
+				}
+				Thread.Sleep(1000);
+				Console.Clear();
+			}
+		}
+	}
+}
---
+++ 09/Quiz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Quiz {
+class Program {
+static void Main(string[] args) {
+Console.WriteLine("Какая команда завершает цикл в C#?");
+
+Console.WriteLine("a. quit");
+Console.WriteLine("b. continue");
+Console.WriteLine("c. break");
+Console.WriteLine("d. exit");
+
+AskQuiz("c");
+}
+
+static void AskQuiz(string correctAnswer) {
+string goOn = "yes";
+while (goOn == "yes") {
+Console.WriteLine();
+Console.WriteLine("Введите ответ (или х для завершения программы)");
+string answer = Console.ReadLine();
+
+if (answer == correctAnswer) {
+Console.WriteLine("Верно!!");
+Thread.Sleep(1000);
+break;
+}
+else if (answer == "x") {
+Console.WriteLine("Завершение программы");
+Thread.Sleep(1000);
+goOn = "no";
+}
+else {
+Console.WriteLine("Неверно!");
+}
+}
+}
+}
+}
