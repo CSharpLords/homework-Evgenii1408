0defdfaec9324f820b765f6bbf651ffd736235f1
Alex <iseldomused@gmail.com>
2017-12-23 02:24:53 +0300

    add 14

---
+++ 14/CopyPasteHell/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyPasteHell {
+	class Program {
+		static void Main(string[] args) {
+			float[] numbers = { 765, 341, 1, -3, 0, 13, -200, 23, 44 };
+
+			numbers[0] = numbers[0] / 5;
+			numbers[1] = numbers[1] / 5;
+			numbers[2] = numbers[2] / 5;
+			numbers[3] = numbers[3] / 5;
+			numbers[4] = numbers[4] / 5;
+			numbers[5] = numbers[5] / 5;
+			numbers[6] = numbers[6] / 5;
+			numbers[7] = numbers[7] / 5;
+			numbers[8] = numbers[8] / 5;
+			Console.ReadLine();
+		}
+	}
+}
---
+++ 14/RelativePrecipitations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelativePrecipitations {
+	class Program {
+		static void Main(string[] args) {
+			int[] precipitations = { 23, 4545, 45, 2, -16, 765, 341, 1, -3, 0 };
+
+			for (int i = 0; i < precipitations.Length; i++) {
+				precipitations[i] = precipitations[i] - precipitations[0];
+				Console.WriteLine(precipitations[i]);
+			}
+			Console.ReadLine();
+		}
+	}
+}
